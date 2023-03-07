0bafedefd0a332946f9ab7123909410505848a3d
Axel Prieto <prietoaxel@gmail.com>
2023-03-07 19:38:02 -0300

    Refactor building struct and datatable to cpp

---
+++ Source/RTS_Game/Buildings/Building.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../Units/ParentUnit.h"
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "UObject/NoExportTypes.h"
+#include "Building.generated.h"
+
+/**
+ *
+ */
+USTRUCT(BlueprintType)
+struct RTS_GAME_API FBuilding : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Cost;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float BuildTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Health;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 InitialLevel;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 LevelUpCost;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float LevelUpTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<TSubclassOf<AParentUnit>> BuildableUnits;
+};
