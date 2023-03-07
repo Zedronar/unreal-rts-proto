19158fa39f7e04c8e3f49fba815d52486344b4d2
Axel Prieto <prietoaxel@gmail.com>
2023-03-07 19:59:44 -0300

    Refactor unit struct and datatable to cpp

---
+++ Source/RTS_Game/Units/Unit.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "UObject/NoExportTypes.h"
+#include "Unit.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct RTS_GAME_API FUnit : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Speed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Cost;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float BuildTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Health;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Damage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Description;
+};
