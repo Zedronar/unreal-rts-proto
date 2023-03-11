494743a0a841601ea9b32bb3c2acd7b871590752
Axel Prieto <prietoaxel@gmail.com>
2023-03-11 11:51:12 -0300

    Refactor player controller properties from blueprint to cpp

--- Source/RTS_Game/Blueprints/RTS_PlayerController.h
+++ Source/RTS_Game/Blueprints/RTS_PlayerController.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "../Buildings/ParentBuilding.h"
+#include "../Buildings/BuildingData.h"
+
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "RTS_PlayerController.generated.h"
@@ -11,4 +14,43 @@ UCLASS()
 class RTS_GAME_API ARTS_PlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+protected:
+	#pragma region BuildingConstruction
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building Construction")
+	bool IsConstructingBuilding = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building Construction")
+	float ConstructionProgress = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building Construction")
+	float ConstructionTimeSpent = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building Construction")
+	float ConstructionTimeNeeded = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building Construction")
+	float ConstructionTimerGranularity = 0.1f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building Construction")
+	TSubclassOf<AParentBuilding> BuildingBeingConstructed;
+	#pragma endregion BuildingConstruction
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FLinearColor TeamColor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 TeamNumber;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool IsLeftMousePressed = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector RightClickLocation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 ResourceAmount = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	AParentBuilding* SelectedBuilding;
 };
