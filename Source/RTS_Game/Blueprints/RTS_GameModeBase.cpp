7fedb69f9103ddf3581e1ce5eb9e12aa8817325c
Axel Prieto <prietoaxel@gmail.com>
2023-03-13 17:54:46 -0300

    Move RTS_GameModeBase logic from blueprint to cpp

--- Source/RTS_Game/Blueprints/RTS_GameModeBase.cpp
+++ Source/RTS_Game/Blueprints/RTS_GameModeBase.cpp
@@ -1,2 +1,29 @@
 #include "RTS_GameModeBase.h"
 
+ARTS_GameModeBase::ARTS_GameModeBase()
+{
+	// Get all player start camps from world
+	TArray<AActor*> OutActors = TArray<AActor*>();
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerStartCamp::StaticClass(), OutActors);
+	PlayerStartCamps = reinterpret_cast<const TArray<APlayerStartCamp*>&>(OutActors);
+}
+
+void ARTS_GameModeBase::HandleStartingNewPlayer_Implementation(APlayerController* NewPlayer)
+{
+	// Select a random player start camp
+	SelectedPlayerStartCamp = PlayerStartCamps[FMath::RandRange(0, PlayerStartCamps.Num() - 1)];
+	
+	// Spawn camera pawn
+	ACameraPawn* NewCameraPawn = GetWorld()->SpawnActor<ACameraPawn>(
+		ACameraPawn::StaticClass(),
+		SelectedPlayerStartCamp->CameraLocationStart->GetComponentTransform()
+	);
+	NewPlayer->Possess(NewCameraPawn);
+
+	// Send player starting info to player controller
+	II_RTS::Execute_SetupPlayerStart(NewPlayer, SelectedPlayerStartCamp, TeamNumber, TeamColors[TeamNumber]);
+
+	// Increment team number and remove player start camp
+	TeamNumber++;
+	PlayerStartCamps.Remove(SelectedPlayerStartCamp);
+}
--- Source/RTS_Game/Blueprints/RTS_GameModeBase.h
+++ Source/RTS_Game/Blueprints/RTS_GameModeBase.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include "PlayerStartCamp.h"
+#include "CameraPawn.h"
+#include "../Interfaces/I_RTS.h"
+
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Kismet/GameplayStatics.h"
 #include "RTS_GameModeBase.generated.h"
 
 /**
@@ -11,5 +16,22 @@ UCLASS()
 class RTS_GAME_API ARTS_GameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	ARTS_GameModeBase();
+
+	virtual void HandleStartingNewPlayer_Implementation(APlayerController* NewPlayer) override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<APlayerStartCamp*> PlayerStartCamps;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	APlayerStartCamp* SelectedPlayerStartCamp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 TeamNumber;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FLinearColor> TeamColors = { FLinearColor::Blue , FLinearColor::Red, FLinearColor::Green, FLinearColor::Yellow};
 };
